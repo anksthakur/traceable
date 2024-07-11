dbfe842129e876a329d0c22c4e54f08b35b3f208
anup thakur <anksthakur7@gmail.com>
2024-07-11 18:32:03 +0530

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,18 @@
-export function middleware(){
-    
-}
+// import { NextResponse } from "next/server";
+// import { getToken } from "next-auth/jwt";
+
+// export async function middleware(request: any) {
+//   const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+
+//   // If user is not authenticated and is trying to access a path other than "/", redirect to "/"
+//   if (!token && request.nextUrl.pathname !== "/") {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
+
+//   // / hai to he chle by
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: '/((?!_next|favicon.ico).*)', // Apply middleware to all paths except _next and favicon.ico
+// };
